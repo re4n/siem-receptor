8a42ecfd58e00ffbe094013464fd9b42aba31820
Ryan Rouxinol <r.rouxinol02@gmail.com>
2026-02-25 20:15:57 -0300

    feat: initial setup siem-receptor

---
+++ src/main/java/com/re4n/siem_receptor/SiemReceptorApplication.java
@@ -0,0 +1,13 @@
+package com.re4n.siem_receptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemReceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiemReceptorApplication.class, args);
+	}
+
+}
